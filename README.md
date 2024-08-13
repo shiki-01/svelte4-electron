7ded3a842808f1f92921334989772c584d10ed32
shiki-01 <0110shiki@gmail.com>
2024-08-13 21:30:17 +0900

    README.md

--- README.md
+++ README.md
@@ -1,3 +1,82 @@
-# svelte4 + electron app
+# svelte4 + electron
 
-## usage
+## Overview
+
+This project is a template for building desktop applications using Svelte 4 and Electron. It combines the reactive UI capabilities of Svelte with the powerful desktop application framework Electron.
+
+## Features
+
+- **Svelte 4**: A modern JavaScript framework for building fast and reactive user interfaces.
+- **Electron**: A framework for building cross-platform desktop applications with web technologies.
+- **Vite**: A fast build tool and development server for modern web projects.
+- **TypeScript**: Strongly typed programming language that builds on JavaScript.
+
+... and tailwind
+
+## Usage
+
+### Prerequisites
+
+- Node.js (version 14 or higher)
+- npm (version 6 or higher)
+
+### Installation
+
+#### To use GitHub template
+
+1. Click the "Use this template" button on the GitHub repository page.
+2. Enter the repository name and click the "Create repository from template" button.
+3. Clone the repository to your local machine.
+4. Install the dependencies by running `npm install`.
+
+#### To clone the repository
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/your-username/svelte4-electron-app.git
+   cd svelte4-electron-app
+    ```
+
+2. Install the dependencies:
+   ```sh
+   npm install
+   ```
+
+### Development
+
+To start the development server, run:
+```sh
+npm run dev
+```
+
+This will start the Vite development server and open the Electron application window.
+
+### Building
+
+To build the application, run:
+```sh
+npm run build
+```
+
+This will build the Svelte application and package it into an Electron application.
+
+## Scripts
+
+* `dev`: Starts the development server and Electron application.
+* `build`: Builds the Svelte application for production.
+* `build:win`: Builds the application for Windows.
+* `build:mac`: Builds the application for macOS.
+* `build:linux`: Builds the application for Linux.
+* `build:all`: Builds the application for both Windows and macOS.
+* `preview`: Previews the production build.
+* `check`: Runs type checking and linting.
+* `check:watch`: Runs type checking and linting in watch mode.
+* `format`: Formats the code using Prettier.
+* `lint`: Lints the code using ESLint.
+
+## Change to Yours
+
+1. Change the package name, description, author, and repository in `package.json`.
+2. Change the application name in `build.config.json`.
+3. Change the license in `LICENSE`.
+4. Change the awesome app in your hand !!!
--- src/electron.js
+++ src/electron.js
@@ -15,11 +15,11 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 if (process.env.NODE_ENV === 'development') {
- try {
-  require('electron-reloader')(module, {});
+	try {
+		require('electron-reloader')(module, {});
 	} catch {
-  console.log('Error: electron-reloaded not found');
- }
+		console.log('Error: electron-reloaded not found');
+	}
 }
 
 const port = process.env.PORT || 5173;
@@ -27,107 +27,107 @@ const dev = !app.isPackaged;
 let mainWindow;
 
 function createWindow() {
- let windowState = windowStateManager({
-  defaultWidth: 800,
-  defaultHeight: 600,
- });
+	let windowState = windowStateManager({
+		defaultWidth: 800,
+		defaultHeight: 600,
+	});
 
- const mainWindow = new BrowserWindow({
-  backgroundColor: 'whitesmoke',
-  titleBarStyle: 'hidden',
-  autoHideMenuBar: true,
-  trafficLightPosition: {
-   x: 17,
-   y: 32,
-  },
-  minHeight: 450,
-  minWidth: 500,
-  webPreferences: {
-   enableRemoteModule: true,
-   contextIsolation: true,
-   nodeIntegration: true,
-   spellcheck: false,
-   devTools: dev,
-   preload: path.join(__dirname, 'preload.cjs'),
-  },
-  x: windowState.x,
-  y: windowState.y,
-  width: windowState.width,
-  height: windowState.height,
- });
+	const mainWindow = new BrowserWindow({
+		backgroundColor: 'whitesmoke',
+		titleBarStyle: 'hidden',
+		autoHideMenuBar: true,
+		trafficLightPosition: {
+			x: 17,
+			y: 32,
+		},
+		minHeight: 450,
+		minWidth: 500,
+		webPreferences: {
+			enableRemoteModule: true,
+			contextIsolation: true,
+			nodeIntegration: true,
+			spellcheck: false,
+			devTools: dev,
+			preload: path.join(__dirname, 'preload.cjs'),
+		},
+		x: windowState.x,
+		y: windowState.y,
+		width: windowState.width,
+		height: windowState.height,
+	});
 
- enable(mainWindow.webContents);
+	enable(mainWindow.webContents);
 
- windowState.manage(mainWindow);
+	windowState.manage(mainWindow);
 
- mainWindow.once('ready-to-show', () => {
-  mainWindow.show();
-  mainWindow.focus();
- });
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+		mainWindow.focus();
+	});
 
- mainWindow.on('close', () => {
-  windowState.saveState(mainWindow);
- });
+	mainWindow.on('close', () => {
+		windowState.saveState(mainWindow);
+	});
 
- return mainWindow;
+	return mainWindow;
 }
 
 contextMenu({
- showLookUpSelection: false,
- showSearchWithGoogle: false,
- showCopyImage: false,
- prepend: () => [
-  {
-   label: 'Make App 💻',
-  },
- ],
+	showLookUpSelection: false,
+	showSearchWithGoogle: false,
+	showCopyImage: false,
+	prepend: () => [
+		{
+			label: 'Make App 💻',
+		},
+	],
 });
 
 function loadVite(port) {
- mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
-  console.log('Error loading URL, retrying', e);
-  setTimeout(() => {
-   loadVite(port);
-  }, 200);
- });
+	mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
+		console.log('Error loading URL, retrying', e);
+		setTimeout(() => {
+			loadVite(port);
+		}, 200);
+	});
 }
 
 function createMainWindow() {
- mainWindow = createWindow();
- mainWindow.once('close', () => {
-  mainWindow = null;
- });
+	mainWindow = createWindow();
+	mainWindow.once('close', () => {
+		mainWindow = null;
+	});
 
- if (dev) loadVite(port);
+	if (dev) loadVite(port);
 }
 
 app.once('ready', () => {
- createMainWindow();
+	createMainWindow();
 });
 app.on('activate', () => {
- if (!mainWindow) {
-  createMainWindow();
- }
+	if (!mainWindow) {
+		createMainWindow();
+	}
 });
 app.on('window-all-closed', () => {
- if (process.platform !== 'darwin') app.quit();
+	if (process.platform !== 'darwin') app.quit();
 });
 
 ipcMain.on('to-main', (event, count) => {
- return mainWindow.webContents.send('from-main', `next count is ${count + 1}`);
+	return mainWindow.webContents.send('from-main', `next count is ${count + 1}`);
 });
 
 // window
 ipcMain.on('window-close', () => {
- mainWindow.close();
+	mainWindow.close();
 });
 ipcMain.on('window-minimize', () => {
- mainWindow.minimize();
+	mainWindow.minimize();
 });
 ipcMain.on('window-maximize', () => {
- if (mainWindow.isMaximized()) {
-  mainWindow.unmaximize();
- } else {
-  mainWindow.maximize();
- }
+	if (mainWindow.isMaximized()) {
+		mainWindow.unmaximize();
+	} else {
+		mainWindow.maximize();
+	}
 });
